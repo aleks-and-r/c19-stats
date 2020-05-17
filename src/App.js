ba9e4caf9b4f394096bdb27c5e3e366bc3d18ade
aleks-and-r <aleksandar.kosanovic88@gmail.com>
2020-05-17 11:50:06 +0200

    C19-stats

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./App.css";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+import imagec from "./images/image.png";
+import { Cards, Chart, CountryPicker } from "./components";
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={imagec} alt="covid-image" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
